1e981fda6e7f798bfb79da80ffe8d8bb202bad1c
dj0024javia <31767853+dj0024javia@users.noreply.github.com>
2020-08-29 22:59:24 +0530

    final css done and deployed over firebase

--- fb_messenger_clone/src/App.js
+++ fb_messenger_clone/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button'
-import { FormControl, InputLabel, Input } from '@material-ui/core';
+import { FormControl, InputLabel, Input, IconButton } from '@material-ui/core';
 import Message from './Message';
 import { db } from './firebase';
 import firebase from 'firebase'
-
+import FlipMove from 'react-flip-move';
+import SendIcon from '@material-ui/icons/Send';
 
 function App() {
   const [input, setInput] = useState('')
@@ -28,7 +29,7 @@ function App() {
   useEffect(() => {
     db.collection('fb-messenger-clone').orderBy('timestamp','desc').onSnapshot(snapshot => {
       
-      setMsgs(snapshot.docs.map(doc => doc.data())
+      setMsgs(snapshot.docs.map(doc => ({id: doc.id , data: doc.data()}))
       )
     })
 
@@ -48,30 +49,40 @@ function App() {
   return (
 
     <div className="App">
-      <h1>Welcome to FB Messenger</h1>
-
+      <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" />
+      <h1>Welcome to FB Messenger {username}!</h1>
+      
       {/* Components Required
       1. Input field
       2. Send button
       3. Messages incoming and outgoing */}
-      <form id="chat-console" onSubmit={formsubmit}>
-        <FormControl>
+      <form id="chat-console" className="app__form" onSubmit={formsubmit}>
+        <FormControl className="app__formcontrol">
           {/* 1. Input Field */}
-          <InputLabel htmlFor="my-input">{username}</InputLabel>
-          <Input id="my-input" aria-describedby="my-helper-text" label="Enter Message" placeholder="Type Your message here" value={input} onChange={e => 
+          {/* <InputLabel htmlFor="my-input">{username}</InputLabel> */}
+          <Input className="app_formcontrol_input" id="my-input" aria-describedby="my-helper-text" placeholder="Type Your message here" value={input} onChange={e => 
           setInput(e.target.value)
           } />
           {/* 2. Button */}
-          <Button variant="contained" color="primary" type="submit" disabled={!input} onClick={sendMessage}>Send</Button>
+          {/* <Button variant="contained" color="primary" type="submit" disabled={!input} onClick={sendMessage}>Send</Button> */}
+          <IconButton className="app_formcontrol_iconbutton" type="submit" disabled={!input} onClick={sendMessage}>
+            <SendIcon />
+          </IconButton>
         </FormControl>
       </form>
       {/* 3. Message Incoming and Outgoing. */}
-      
-      {
-        msgs.map(eachMessage => 
-          <Message object={eachMessage} username={username}/>
-          )
-      }
+      <FlipMove>
+        {
+          msgs.map(eachMessage =>
+            (<Message key={eachMessage.id} object={eachMessage.data} username={username}/>)
+            )
+        }
+        {/* So Above part is bit complicated to understand. Here we are passing eachMessage as an element from a dictionary of documents. 
+        So, eachMessage has 2 elements, id and data. Go to onSnapshot part of the code and there you can see 2 keys of eachMessage element
+        Now, here we were passing message object before which had 3 attributes, message, username and timestamp.
+        Here we have converted that dict into another object(element of bigger dict) which is part of a dict(msgs)
+        key property is required to prevent whole thing from re-rendering. it only renders new msg being pushed */}
+      </FlipMove>
 
     </div>
   );
--- fb_messenger_clone/src/Message.js
+++ fb_messenger_clone/src/Message.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { Card, CardContent, Typography } from '@material-ui/core'
 import './Message.css'
 
-function Message({object, username}) {
+
+// forwardRef is used to render things in a more stylish way. ref is used for reference to the tags that in particular to be
+// used as rendering.
+const Message = forwardRef( ({object, username}, ref) => {
 
     const isUser = (username === object.username);
 
     return (
-        <div className={`message ${isUser && 'message__user'}`}>
+        <div ref={ref} className={`message ${isUser && 'message__user'}`}>
             <Card className={isUser ? "message__userclass" : "message__defaultclass"}>
                 <CardContent>
                     <Typography variant="h6" component='h2' color='white'>
-                        {object.username}: {object.message}
+                        {!isUser && `${object.username || 'Unknown User'} says:`} {object.message}
                     </Typography>
                 </CardContent>
             </Card>
         </div>
     )
-}
+})
 
 export default Message
